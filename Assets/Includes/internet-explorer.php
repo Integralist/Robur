fc9cad1a8dbf5519eba1c7c2c1e8e3c2ec4aa31e
Mark McDonnell <mark.mcdx@gmail.com>
2012-11-24 20:47:13 +0000

    Tidy up

--- Assets/Includes/internet-explorer.php
+++ Assets/Includes/internet-explorer.php
@@ -1,7 +1,7 @@
 <!--[if lte IE 7]>
     <link rel="stylesheet" media="screen" href="/Assets/Styles/IE_notification.css" />
-    <div id="ie-container"></div>
-    <div id="ie-message">
+    <div class="ie-container"></div>
+    <div class="ie-message">
     	<a href="/internet-explorer/">
     		<span>Internet Explorer 6/7</span>
     		<img src="/Assets/Images/Browser-Warning-Message.png">
