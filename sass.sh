47b31e63d2cf07a7a0bf782e340458d17578f54b
Mark McDonnell <mark.mcdx@gmail.com>
2012-11-24 20:39:58 +0000

    Updated Sass bash script to include debug information

--- sass.sh
+++ sass.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-sass --style compressed --watch Assets/Styles/Sass:Assets/Styles
+#sass --style expanded --watch Assets/Styles/Sass:Assets/Styles --debug-info
+#sass --style compressed --watch Assets/Styles/Sass:Assets/Styles
+
+sass --style expanded --watch Assets/Styles/Sass:Assets/Styles --debug-info
 
 exit 0
